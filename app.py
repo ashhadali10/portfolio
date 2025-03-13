bcd69a3187b9fead790c49d219364e7bbf4bc5e0
ashha <ashha@localhost.localdomain>
2025-03-13 22:08:20 +0500

    add html css file

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask  
 app = Flask(__name__)  
 
+#homepage
 @app.route('/')  
 def home():  
     return "Hello, Worlddd! This is my DevOps Portfolio!"  
+# about page
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':  
     app.run(host='0.0.0.0', port=5000)
